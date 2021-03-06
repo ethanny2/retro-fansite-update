264413684b8c30ff87aa3580e91691225658ac9c
Ethan <ethanny2@gmail.com>
2021-03-06 13:27:13 -0500

    Added new video attributes to see if the muted autoplay works on mobile devices. Removed the pace loading bar as it infintely hangs. Will work on getting the mobile 320px of the music player to work

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,4 @@
-import Pace from "../js/pace";
-Pace.start();
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../sass/pace.scss";
 import "../sass/main_styling.scss";
 // May need to remove this if not purgeCSS will
 import "bootstrap";
